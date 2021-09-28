752a295b5bee7032ca9f8699fbfd0986aefeebc7
toomuchpie94 <alex.s.kim.94@gmail.com>
2021-09-28 11:27:17 -0500

    Added add new book button, remove book button, and added CSS shell

--- main.js
+++ main.js
@@ -5,15 +5,24 @@ function library(owner) {
 }
 //Attaches locations to books in library for easy reference
 library.prototype.assignBookLocation = function() {
-    for(i in myLibrary.books){
-        myLibrary.books[i].libraryOrder = i;
+    for(i in this.books){
+        this.books[i].libraryOrder = i;
     }
+}
+
+//function to add new book
+library.prototype.addBookToLibrary = function(book) {
+    let librarySize = this.books.length;
+    this.books.push(book);
+    book.library = this;
+    book.libraryOrder = librarySize;
 }
 
 //book constructor
 function book(title, author) {
     this.title = title;
     this.author = author;
+    this.library = '';
     this.libraryOrder = 0; //determines location in library for easy removal
     this.read = false;
 }
@@ -23,19 +32,13 @@ book.prototype.markAsRead = function() {
 }
 //removes book from library
 book.prototype.removeBookFromLibrary = function() {
-    myLibrary.books.splice(this.libraryOrder,1);
-    myLibrary.assignBookLocation();
+    this.library.books.splice(this.libraryOrder,1);
+    this.library.assignBookLocation();
 }
 
-//function to add new book
-function addBookToLibrary(book) {
-    let librarySize = myLibrary.books.length;
-    myLibrary.books.push(book);
-    book.libraryOrder = librarySize;
-}
+
 
 /* LIBRARY UI */
-
 
 //library DOM
 function createLibraryDOM(library){
@@ -45,16 +48,65 @@ function createLibraryDOM(library){
     let libraryOwnerDOM = document.createElement('h1');
     libraryOwnerDOM.className = "libraryowner";
     libraryOwnerDOM.textContent = library.owner;
+    let bookShelfDOM = document.createElement('div');
+    bookShelfDOM.className = "bookshelf";
+
+    //add book button will be on shelf
     let addBookButton = document.createElement('button');
     addBookButton.id = "addbookbutton";
     addBookButton.textContent = "+";
     addBookButton.addEventListener('click', function(){
-        newBookForm()
-    })
-    let bookShelfDOM = document.createElement('div');
-    bookShelfDOM.className = "bookshelf";
+        let newBook = createNewBook(library, bookShelfDOM);
+        bookShelfDOM.replaceChild(newBook, addBookButton);
+    });
+    bookShelfDOM.appendChild(addBookButton);
 
-    libraryDOM.append(libraryOwnerDOM, addBookButton, bookShelfDOM);
+    libraryDOM.append(libraryOwnerDOM, bookShelfDOM);
+    
+    return libraryDOM;
+}
+
+//Form for creating new book
+function createNewBook(library, bookShelfDOM){
+    let newBookForm = document.createElement('div');
+    newBookForm.className = "book";
+
+    let titleInput = document.createElement('input');
+    titleInput.type = 'text';
+    titleInput.placeholder = "New Title"
+    titleInput.className = "titleinput"
+    let authorInput = document.createElement('input');
+    authorInput.type = 'text';
+    authorInput.placeholder = 'New Author'
+    authorInput.className = "authorinput"
+    let submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit'
+    submitButton.id = 'submitbutton'
+    submitButton.addEventListener('click', function(){
+        //creates new book given inputted title and author in place of input form
+        let title = titleInput.value;
+        let author = authorInput.value;
+        let newBook = new book(title, author);
+        library.addBookToLibrary(newBook);
+        let newBookDOM = createBookDOM(newBook);
+        newBookForm.parentNode.replaceChild(newBookDOM, newBookForm);
+
+        //adds addBookButton at the end of the library
+        let addBookButton = document.createElement('button');
+        addBookButton.id = "addbookbutton";
+        addBookButton.textContent = "+";
+        addBookButton.addEventListener('click', function(){
+            let newBook = createNewBook(library, bookShelfDOM);
+            bookShelfDOM.replaceChild(newBook, addBookButton);
+        });
+        bookShelfDOM.appendChild(addBookButton);
+    })
+
+    
+
+    newBookForm.append(titleInput,authorInput,submitButton);
+    
+    return newBookForm;
 }
 
 //creates a book DOM
@@ -72,15 +124,29 @@ function createBookDOM(book){
     readDOM.type = "checkbox";
     readDOM.className = "readstatus";
     readDOM.checked = book.read;
-    readDOM.addEventListener('change', function() {
+    readDOM.addEventListener('change', function(){
         book.read = readDOM.checked;
     });
-    //remove book button
 
-    bookDOM.append(titleDOM, authorDOM, readDOM);
+    //remove book button
+    let removeBookButton = document.createElement('button');
+    removeBookButton.className = 'removebook'
+    removeBookButton.textContent = 'x';
+    removeBookButton.addEventListener('click', function(){
+        book.removeBookFromLibrary();
+        bookDOM.parentNode.removeChild(bookDOM);
+    })
+
+    bookDOM.append(titleDOM, authorDOM, readDOM, removeBookButton);
 
     return bookDOM;
 }
 
 
+
+
+let content = document.querySelector('#content');
+let myLibrary = new library('alex');
+let myLibraryDOM = createLibraryDOM(myLibrary);
+content.appendChild(myLibraryDOM)
 
